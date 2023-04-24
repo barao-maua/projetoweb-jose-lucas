334cc4f987a2abcf4c03697ed391b4b2a8339efb
ZeVitorR <rodriguesze6@gmail.com>
2023-04-24 07:10:36 -0300

    instalação e settings

--- HoraAula/HoraAula/urls.py
+++ HoraAula/HoraAula/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Cronograma import views
 
 urlpatterns = [
+    path ('index/', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
+
+
