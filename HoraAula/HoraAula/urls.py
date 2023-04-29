49000853565c9a241972b0fbbf10e50af7fbedf5
ZeVitorR <rodriguesze6@gmail.com>
2023-04-29 11:54:37 -0300

    E4_ArquivosStatic

--- HoraAula/HoraAula/urls.py
+++ HoraAula/HoraAula/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Cronograma import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path ('', views.index, name='index'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
