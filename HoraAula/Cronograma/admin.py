e5b4a802cddd1e75518de79838a4f527fa480059
ZeVitorR <rodriguesze6@gmail.com>
2023-05-11 12:52:37 -0300

    E4-MODEL

--- HoraAula/Cronograma/admin.py
+++ HoraAula/Cronograma/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Professor)
+admin.site.register(models.Materia)
+admin.site.register(models.Horario)
--- HoraAula/Cronograma/models.py
+++ HoraAula/Cronograma/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    nome = models.CharField(verbose_name="Nome", max_length=200,null=False, help_text="Nome do Professor" )
+    nomeAbreviado = models.CharField(verbose_name="NomeAbreviado", max_length=50, null=False, help_text="Nome Abreviado do Professor")
+    cpf = models.CharField(verbose_name="CPF",null=False, max_length = 11, help_text="CPF do professor")
+    endereco = models.CharField(verbose_name="Endereco",null=False, max_length=100, help_text="Endereço do professor: Rua e Numero")
+    cidade = models.CharField(verbose_name="Cidade", max_length=100,null=False, help_text="Cidade do professor")
+    cep = models.CharField(verbose_name="CEP",max_length=8,null=False, help_text="Cep da cidade do professor")
+
+    def __str__(self):
+        return self.nome
+    
+class Materia(models.Model):
+    nome = models.CharField(verbose_name="Nome", max_length=200, null=False, blank=False,help_text="Nome da materia" )
+    descricao = models.CharField(verbose_name="Descricao", max_length=2000, null=False, blank=False, help_text="Descreva a materia")
+    curso = models.CharField(verbose_name="Curso", max_length=50, null=False,blank=False,help_text="Curso da materia")
+    semestre = models.IntegerField(verbose_name="Semestre", null=False, help_text="Numero do semestre que é a materia")
+    professor = models.ForeignKey(
+        Professor,
+        on_delete=models.CASCADE,
+        verbose_name="Professor",
+        null=False,
+        help_text="Materia que o professor ensina"
+    )
+
+    def __str__(self):
+        return self.nome
+
+class Horario(models.Model):
+    dia = models.CharField(verbose_name="Dia", max_length=200, null=False, blank=False,help_text="Dia da semana da disciplina" )
+    horario = models.CharField(verbose_name="horario", max_length=5, null=False, blank=False,help_text="Horario da disciplina")
+    professor = models.ForeignKey(
+        Professor,
+        on_delete=models.CASCADE,
+        verbose_name="Professor",
+        null=False,
+        help_text="horario da aula do professor"
+    )
+    materia = models.ForeignKey(
+        Materia,
+        on_delete=models.CASCADE,
+        verbose_name="Materia",
+        null=False,
+        help_text="horario da materia"
+    )
