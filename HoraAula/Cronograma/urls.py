333152d4afe37e3b22e30110b0ed5f341bc5bb14
ZeVitorR <rodriguesze6@gmail.com>
2023-05-19 19:39:14 -0300

    E5_termindo templates

--- HoraAula/Cronograma/urls.py
+++ HoraAula/Cronograma/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from Cronograma import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path ('', include("Cronograma.urls")),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
+    path ('', views.index, name='index'),
+    path ('horarios', views.index, name='horario'),
+    path ('horario/inserir', views.index, name='inserir'),
+    path ('horario/alterar', views.index, name='alterar'),
+    path ('horario/deletar', views.index, name='deletar'),
 ] 
+
 
--- HoraAula/Cronograma/views.py
+++ HoraAula/Cronograma/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render 
 from django.http import HttpResponse 
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from .models import Horario
 
 def index(request): 
     return render(request, 'index.html')
+
+
+class HorarioListView(generic.ListView):
+    model = Horario
+    template_name = 'horario.html'
+    context_object_name = 'horario'
+
+class HorarioCreateView(generic.CreateView):
+    model = Horario
+    template_name = 'create.html'
+    context_object_name = 'horario'
